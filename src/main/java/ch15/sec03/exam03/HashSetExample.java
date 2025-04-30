8faf9dda387a9cfe7f125ce821e02de6e43cef7d
HyoSangByun <billy04100410@gmail.com>
2025-04-30 15:48:12 +0900

    20250430 기본 과제

---
+++ src/main/java/ch15/sec03/exam03/HashSetExample.java
@@ -0,0 +1,28 @@
+package ch15.sec03.exam03;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<String>();
+
+        set.add("java");
+        set.add("JDBC");
+        set.add("JSP");
+        set.add("Spring");
+
+        Iterator<String> iterator = set.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("================");
+
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+}
